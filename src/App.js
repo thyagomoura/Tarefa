2ee9bdc0466c060f2e10028b4b60ab354712f198
Thyago Moura <61210307+thyagomoura@users.noreply.github.com>
2021-12-18 19:45:16 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Routes from "./routes";
+import Home from './pages/home'
+import Escolha from './pages/escolha'
+// import {Route} from "react-router-dom";
+
+
+function App() {
+  return (
+    <>
+    {/* <Home/> */}
+    <Escolha/>
+    {/* <Route component = { Home }  path="/" exact />
+    <Route component = { Escolha }  path="/escolha" exact /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import { Route, BrowserRouter } from "react-router-dom";
+// import Home from './pages/home/index.js'
+// import Escolha from './pages/escolha/escolha'
+
+// const Routes = () => {
+//    return(
+//        <BrowserRouter>
+//            <Route component = { Home }  path="/" exact />
+//            <Route component = { Escolha }  path="/escolha" exact />
+//        </BrowserRouter>
+//    )
+// }
+
+// export default Routes;
